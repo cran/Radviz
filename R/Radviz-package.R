d3456eb154f98663f221ca4f325337d5d94303ea
Yann Abraham <yann.abraham@gmail.com>
2025-05-09 06:30:02 +0000

    version 0.9.4

---
+++ R/Radviz-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal 
+"_PACKAGE"
+
+#' Radviz Projection of Multidimensional Data
+#'
+#' Radviz uses Dimensional Anchors and the spring paradigm to project a multidimensional space in 2D.
+#' This allows for the quick visualization of large and complex datasets.
+#' 
+#' @example examples/example-do.radviz.R
+#' @examples
+#' plot(rv,anchors.only=FALSE)
+#' 
+#' @name Radviz-package
+NULL
